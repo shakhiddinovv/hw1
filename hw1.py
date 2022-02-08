4018c810ae67531883fbf0380bc227343eed091d
shakhiddinovv <sanjar050@bk.ru>
2022-02-08 14:45:56 +0500

    added 1st hw

---
+++ hw1.py
@@ -0,0 +1,8 @@
+a=(int(input('give the number: ')))
+b=(int(input('give the number: ')))
+c=(int(input('give the number: ')))
+print(id(a), id(b), id(c))
+
+x=(input('name:'))
+y=(input('surname: '))
+print('hello '+  y  + ' ' + x)
